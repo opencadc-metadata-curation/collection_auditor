a026bdd46ba62277c9a006baac3ec3b31205fe80
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2026-02-27 23:42:33 +0000

    CADC-15225 - get the Dockerfile and artifact_diff.sh to agree.

--- audit_tools/artifact_diff.sh
+++ audit_tools/artifact_diff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IMAGE="audit_tools"
+IMAGE="opencadc/audit_tools"
 
 cp ~/.ssl/cadcproxy.pem .
 
 mkdir -p ./collectionAuditing
 
-sudo docker run --rm --user $(id -u):$(id -g) -e HOME=/app -v ${HOME}:/app ${IMAGE} ${@}
+sudo docker run --rm -ti --user $(id -u):$(id -g) -e HOME=/app -v ${PWD}:/app ${IMAGE}  python /usr/local/bin/caomArtifactDiff.py ${@}
