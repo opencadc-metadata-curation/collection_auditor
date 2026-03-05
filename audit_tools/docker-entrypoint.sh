61fa187654400dfbbe520513882200af3f08f83f
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2026-03-04 16:23:14 -0800

    Update docker-entrypoint.sh - make the config files available outside of the container.

--- audit_tools/docker-entrypoint.sh
+++ audit_tools/docker-entrypoint.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+mkdir -p ${PWD}/config
+
+for f_name in caomCollections.tsv caomSiMappings.tsv caomSites.tsv caomMonitoringConfig.xlsx; do
+    if [[ ! -e ${PWD}/config/${f_name} ]]; then
+        echo "Refresh ${f_name}"
+        cp /usr/local/.config/${f_name} ${PWD}/config/
+    fi
+done
+
 exec "${@}"
