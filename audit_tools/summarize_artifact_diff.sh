0a7007aeea8a08d3146918e36771cd9b4bb41cca
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2026-03-18 15:42:57 -0700

    Update summarize_artifact_diff.sh

--- audit_tools/summarize_artifact_diff.sh
+++ audit_tools/summarize_artifact_diff.sh
@@ -4,4 +4,4 @@ IMAGE="opencadc/artifact-diff:3.13-slim"
 
 cp ~/.ssl/cadcproxy.pem .
 
-sudo docker run --rm -ti --user $(id -u):$(id -g) -e HOME=/app -v ${PWD}:/app ${IMAGE} /bin/bash -c "/app/collectionAuditingSummary.sh /app/collectionAuditing/artifactDiff_reports/"
+docker run --rm -ti --user $(id -u):$(id -g) -e HOME=/app -v ${PWD}:/app ${IMAGE} /bin/bash -c "/app/collectionAuditingSummary.sh /app/collectionAuditing/artifactDiff_reports/"
